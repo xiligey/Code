2cdd687af1dfb088f1965fd6074798c32d65f784
chenxilin <chenxilin@bizseer.com>
2021-03-01 20:34:43 +0800

    feat: python spider bs4

--- python/spider_notes/Spider_In_Action/BeautifulSoup.py
+++ python/spider_notes/Spider_In_Action/BeautifulSoup.py
@@ -8,9 +8,9 @@ html = """
 <body>
 <p class="title" name="dromouse"><b> The Dormouse's story</b></p>
 <p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elise" class="sister" id="link1"><!-- Elise --></a>'
-<a href="http://example.com/lacie: class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+<a href="http://example.com/elise" class="sister" id_="link1"><!-- Elise --></a>'
+<a href="http://example.com/lacie: class="sister" id_="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id_="link3">Tillie</a>;
 and they lived at the bottom of a well.</p>
 <p class="story">...</p>
 """
